f90a79885ec62ad847f141d6355ffde9473334c8
osman.basaran <osman.basaran@trendyol.com>
2020-05-19 02:26:33 +0300

    added 404 page support

---
+++ src/components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="jumbotron text-center">
+         <p style="font-size: 5em;color: red;"><img src="/images/404.png" /></p>
+         <p style="font-size: 1.8em;">Nice try but... <a href="/">GO HOME</a></p>
+    </section>
+</template>
+
+<script>
+
+    export default {
+        name: 'NotFound',
+        data: function () {
+            return {
+                error: false
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
